9c5441a1c7c378f5fc252bad649521a8ed21ad66
jyhyun1008 <jayhyun1008@gmail.com>
2025-03-12 15:48:07 +0900

    채찍피티가이러래

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,16 @@ const withPWA = withPWAInit({
   dest: "public",
 });
 
-export default withPWA({
+
+
+export default withPWA(()=>{
   // Your Next.js config
+  const nextConfig = {
+    output: "export",
+    images: {
+      unoptimized: true, // GitHub Pages에서는 SSR 이미지 최적화 미지원
+    },
+    basePath: "/YOUR_REPOSITORY_NAME", // GitHub Pages에서는 필수
+    assetPrefix: "/YOUR_REPOSITORY_NAME/",
+  };
 });
